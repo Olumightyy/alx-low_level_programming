86adf25717eaa8aabc27f8e1bf9eb91ded8e2e20
User <adepojuolumide04@gmail.com>
2022-10-15 02:47:25 -0700

    Haa

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ int main(void)
 	int b;
 	long int c;
 	long long int d;
-	float ;
+	float f;
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
