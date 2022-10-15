7b3dd8532b4c5c6fc275c94e67b0ab10f50b1a80
User <adepojuolumide04@gmail.com>
2022-10-15 03:09:52 -0700

    BETTY!!!!!!

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<unistd.h>
+/**
+ * main - Entry point
+ * ssize_t write(int fd, const void *buf, size_t count);
+ * Return: Answer 1
+ */
+
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\"-Dora Korpar, 2015-10-19\n";
+
+	return (1);
+}
